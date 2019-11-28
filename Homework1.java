76301d15b18e920e3d3173cd9beb060b16494963
Милашевський Олександр <milan7667@gmail.com>
2019-11-28 20:50:13 +0200

    HW#1

---
+++ Homework1.java
@@ -0,0 +1,20 @@
+package Cursor_Education;
+public class Homework1 {
+    public static void main (String [] args) {
+        byte a = 1;
+        short b = 2;
+        int c = a+b;
+        long d = 1234567;
+        double e = 6.67;
+        double f = d/e;
+        double g = d-f;
+        float h = a*b;
+
+        System.out.println (c);
+        System.out.println (f);
+        System.out.println (g);
+        System.out.println (h);
+    }
+
+}
+
